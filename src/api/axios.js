eb88c9eeb8a0d7355baba3d3f630c0526e4c5952
svivek19 <svivek030503@gmail.com>
2025-12-24 12:26:46 +0530

    fix: update axios base URL to point to the production backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://opengenx-ecommerce-task-backend.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
